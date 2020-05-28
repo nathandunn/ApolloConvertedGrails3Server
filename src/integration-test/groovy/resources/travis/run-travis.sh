9a34197f19fdf11509a75f8b1191eedcee932bd9
Nathan Dunn <ndunn@me.com>
2020-05-28 08:17:23 -0700

    fixed directory locations

--- src/integration-test/groovy/resources/travis/run-travis.sh
+++ src/integration-test/groovy/resources/travis/run-travis.sh
@@ -4,7 +4,7 @@ if [[ $TEST_SUITE == "apollo" ]]; then
 fi
 if [[ $TEST_SUITE == "python-apollo" ]]; then
   set -ex
-  cp src/integration-test/resources/travis/python-apollo.travis apollo-config.groovy
+  cp src/integration-test/groovy/resources/travis/python-apollo.travis apollo-config.groovy
   ./grailsw run-app &
   git clone --single-branch --branch add_bulk_gff_loader --depth=1 https://github.com/galaxy-genome-annotation/python-apollo
   cd python-apollo
