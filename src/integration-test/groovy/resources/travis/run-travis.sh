1d2a4a6babab74603f4c8bc2ce3c90bac7329fca
Nathan Dunn <ndunn@me.com>
2020-05-28 10:27:00 -0700

    adding recommended gradle check

--- src/integration-test/groovy/resources/travis/run-travis.sh
+++ src/integration-test/groovy/resources/travis/run-travis.sh
@@ -1,21 +1,29 @@
-#!/usr/bin/env bash
-if [[ $TEST_SUITE == "apollo" ]]; then
-  ./grailsw test-app
-fi
-if [[ $TEST_SUITE == "python-apollo" ]]; then
-  set -ex
-  cp src/integration-test/groovy/resources/travis/python-apollo.travis apollo-config.groovy
-  ./grailsw run-app &
-  git clone --single-branch --branch add_bulk_gff_loader --depth=1 https://github.com/galaxy-genome-annotation/python-apollo
-  cd python-apollo
-  pyenv global 3.6.3
-  sed -i 's|8888|8080/apollo|' `pwd`/test-data/arrow.yml
-  export ARROW_GLOBAL_CONFIG_PATH=`pwd`/test-data/arrow.yml
-  virtualenv .venv
-  . .venv/bin/activate
-  pip install .
-  pip install nose
-  ./bootstrap_apollo.sh --nodocker
-  python setup.py nosetests
-  killall java || true
-fi
+#!/bin/bash
+
+EXIT_STATUS=0
+
+echo "Running tests with geb.env chromeHeadless"
+./gradlew -Dgeb.env=chromeHeadless check || EXIT_STATUS=$? #
+
+#if [[ $TEST_SUITE == "apollo" ]]; then
+#  ./grailsw test-app
+#fi
+#if [[ $TEST_SUITE == "python-apollo" ]]; then
+#  set -ex
+#  cp src/integration-test/groovy/resources/travis/python-apollo.travis apollo-config.groovy
+#  ./grailsw run-app &
+#  git clone --single-branch --branch add_bulk_gff_loader --depth=1 https://github.com/galaxy-genome-annotation/python-apollo
+#  cd python-apollo
+#  pyenv global 3.6.3
+#  sed -i 's|8888|8080/apollo|' `pwd`/test-data/arrow.yml
+#  export ARROW_GLOBAL_CONFIG_PATH=`pwd`/test-data/arrow.yml
+#  virtualenv .venv
+#  . .venv/bin/activate
+#  pip install .
+#  pip install nose
+#  ./bootstrap_apollo.sh --nodocker
+#  python setup.py nosetests
+#  killall java || true
+#fi
+
+exit $EXIT_STATUS
