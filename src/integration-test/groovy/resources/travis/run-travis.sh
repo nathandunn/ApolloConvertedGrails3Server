9781b8c623b61bdbe7d26502a8e43b11343c2ae6
Nathan Dunn <ndunn@me.com>
2020-06-01 15:52:22 -0700

    updated the grails apollo server

--- src/integration-test/groovy/resources/travis/run-travis.sh
+++ src/integration-test/groovy/resources/travis/run-travis.sh
@@ -15,7 +15,8 @@ if [[ $TEST_SUITE == "python-apollo" ]]; then
   ./grailsw run-app &
   git clone --single-branch --branch add_bulk_gff_loader --depth=1 https://github.com/galaxy-genome-annotation/python-apollo
   cd python-apollo
-  pyenv global 3.6.3
+  python --version
+#  pyenv global 3.6.3
   sed -i 's|8888|8080/apollo|' `pwd`/test-data/arrow.yml
   export ARROW_GLOBAL_CONFIG_PATH=`pwd`/test-data/arrow.yml
   virtualenv .venv
