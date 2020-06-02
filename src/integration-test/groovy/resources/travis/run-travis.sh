72bcda8075cf1f1f418c2226d0a029708ade62a3
Nathan Dunn <ndunn@me.com>
2020-06-01 18:06:14 -0700

    updated grails 3

--- src/integration-test/groovy/resources/travis/run-travis.sh
+++ src/integration-test/groovy/resources/travis/run-travis.sh
@@ -5,7 +5,6 @@ EXIT_STATUS=0
 echo "Running test $TEST_SUITE"
 
 if [[ $TEST_SUITE == "apollo" ]]; then
-#  ./grailsw test-app
   echo "Running tests with geb.env chromeHeadless $TEST_SUITE"
   ./gradlew -Dgeb.env=chromeHeadless check || EXIT_STATUS=$? #
 fi
@@ -17,16 +16,12 @@ if [[ $TEST_SUITE == "python-apollo" ]]; then
   cd python-apollo
   sed -i 's|8888|8080/apollo|' `pwd`/test-data/local-arrow.yml
   export ARROW_GLOBAL_CONFIG_PATH=`pwd`/test-data/local-arrow.yml
-#  sudo pip3 install virtualenv pathlib2
   python3 --version
   python3 -m venv .venv
-#  virtualenv .venv
   . .venv/bin/activate
   python3 --version
   pip3 install .
   pip3 install nose
-#  pip install nose
-#  pip install .
   ./bootstrap_apollo.sh --nodocker
   python3 setup.py nosetests
   killall java || true
