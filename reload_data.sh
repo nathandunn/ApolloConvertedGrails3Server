fd660a65945d689de314e960080f0535e786d619
Nathan Dunn <ndunn@me.com>
2020-08-12 13:54:39 -0700

    removed mostly unused

--- reload_data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-./stop_docker.sh  && rm -rf apollo_data/* && \
-rm -rf $HOME/neo4j/data/*  && \
-./start_docker.sh && \
-./grailsw run-app
--- stop_docker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker stop neo4j
